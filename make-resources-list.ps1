7d59bac097a95aa48f04d370f72109b5e42d8021
Len Kim <cosmolen@gmail.com>
2020-09-09 20:16:06 +0900

    update for patch 5.2 (KR)

--- make-resources-list.ps1
+++ make-resources-list.ps1
@@ -3,7 +3,7 @@ Set-Location $cd
 
 $hashList = Join-Path $cd "resources.txt"
 
-$baseUri = "https://raw.githubusercontent.com/anoyetta/ACT.Hojoring.Resources/master/"
+$baseUri = "https://raw.githubusercontent.com/cosmolen/ACT.Hojoring.Resources/master/"
 
 Write-Output "# Hojoring.Resources" > $hashList
 
@@ -20,12 +20,14 @@ foreach ($f in $files) {
         continue
     }
 
-    $relayPath = Resolve-Path $f -Relative
+    $relayPath = Resolve-Path $f.FullName -Relative
+
+    Write-Output $relayPath
 
     $localPath = $relayPath.Replace(".\", "")
     $remoteUri = $baseUri + $localPath.Replace("\", "/")
 
-    $hash = Get-FileHash $f -Algorithm MD5
+    $hash = Get-FileHash $f.FullName -Algorithm MD5
 
     if ($prevDirectory -ne $f.Directory.ToString()) {
         Write-Output "" >> $hashList
